79cc6d04f5e5a0edad95da4fcec31cdc242b3764
devinhidayat <75529875+devinhidayat@users.noreply.github.com>
2021-03-06 22:55:28 +0700

    part2

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,8 +3,58 @@
         <div class="main-content">
             <div class="wrapper">
                 <h1>Manage Admin</h1>
+                <br /> <br />
 
+                <!-- <?php
+                    if(isset($_SESSION['add'])){
+                        echo $_SESSION['add'];
+                    }
+                ?> -->
+
+
+                <br /><br />
                 
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                    </tr>
+
+                    <tr>
+                        <td>1. </td>
+                        <td>Devin Hidayat</td>
+                        <td>Devin</td>
+                    </tr>
+
+                    <tr>
+                        <td>2. </td>
+                        <td>Edgar Oscar</td>
+                        <td>Edgar</td>
+                        <td>
+                    </tr>
+
+                    <tr>
+                        <td>3. </td>
+                        <td>Unknown</td>
+                        <td>Unknown</td>
+                        <td>
+                    </tr>
+
+                    <tr>
+                        <td>4. </td>
+                        <td>Unknown</td>
+                        <td>Unknown</td>
+                        <td>
+                    </tr>
+
+                    <tr>
+                        <td>5. </td>
+                        <td>Unknown</td>
+                        <td>Unknown</td>
+                        <td>
+                    </tr>
+                </table>
             </div>
         </div>
 
