c29bc65e0364c5f40d4c3a51ea7baadf54a6971f
EdgarEvert <edgaroscar78@gmail.com>
2021-03-08 19:50:51 +0700

    part 5

--- admin/add-food.php
+++ admin/add-food.php
@@ -1,128 +1,109 @@
-<?php include('partials/menu.php'); ?>
+<?php include('partials/menu.php') ?>
 
-    <div class="main-content">
-        <div class="wrapper">
-            <h1>Add Food</h1>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Food</h1>
+        <br><br>
 
-            <br> <br>
+        <?php
+            if(isset($_SESSION['upload'])) {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+        ?>
 
-            <?php
-                if(isset($_SESSION['upload']))
-                {
-                    echo $_SESSION['upload'];
-                    unset($_SESSION['upload']);
-                }
-            ?>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table class="tbl-30">
+                <tr>
+                    <td>Title: </td>
+                    <td>
+                        <input type="text" name="title" placeholder="Title of the Food">
+                    </td>
+                </tr>
 
-            <form action="" method="POST" enctype="multipart/form-data">
+                <tr>
+                    <td>Description: </td>
+                    <td>
+                        <textarea name="description" cols="30" rows="5" placeholder="Description of the Food"></textarea>
+                    </td>
+                </tr>
 
-                <table class="tbl-30">
+                <tr>
+                    <td>Price: </td>
+                    <td>
+                        <input type="number" name="price">
+                    </td>
+                </tr>
 
-                    <tr>
-                        <td>Title: </td>
-                        <td>
-                            <input type="text" name="title" placeholder="Title of the Food">
-                        </td>
-                    </tr>
+                <tr>
+                    <td>Select Image: </td>
+                    <td>
+                        <input type="file" name="image">
+                    </td>
+                </tr>
 
-                    <tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Food" class="btn-secondary">
+                    </td>
+                </tr>
+            </table>
+        </form>
 
-                        <td>Description: </td>
-                        <td>
-                            <textarea name="description" cols="30" rows="5" placeholder="Descriptoon of the Food"></textarea>
-                        </td>
-
-                    </tr>
-
-                    <tr>
-
-                        <td>Price: </td>
-                        <td>
-                            <input type="number" name="price">
-                        </td>
-
-                    </tr>
-
-                    <tr>
-
-                        <td>Select Image: </td>
-                        <td>
-                            <input type="file" name="image">
-                        </td>
-
-                    </tr>
-
-                    <tr>
-
-                        <td colspan="2">
-                            <input type="submit" name="submit" value="Add Food" class="btn-secondary">
-                        </td>
-
-                    </tr>
-
-                </table>
-
-            </form>
-
-            <?php
-                if(isset($_POST['submit']))
+        <?php 
+            if(isset($_POST['submit'])) {
 
                 $title = $_POST['title'];
                 $description = $_POST['description'];
                 $price = $_POST['price'];
 
-                if(isset($_FILES['image']['name']))
-                {
+                if(isset($_FILES['image']['name'])) {
                     $image_name = $_FILES['image']['name'];
 
-                    if($image_name!= "")
-                    {
+                    if($image_name!="") {
                         $ext = end(explode('.', $image_name));
 
                         $image_name = "Food-Name-".rand(0000,9999).".".$ext;
 
-                        $src = $_FILES['image']['tmp_name'];
+                        $src=$_FILES['image']['tmp_name'];
 
                         $dst = "../images/food/".$image_name;
 
                         $upload = move_uploaded_file($src, $dst);
 
-                        if($upload==false)
-                        {
-                            $_SESSION['upload'] = "<div class='error'>Failed to upload image.</div>";
-                            header('location:' .SITEURL.'admin/add-food.php');
+                        if($upload==false) {
+                            $_SESSION['upload'] = "<div class='error'>Failed to Upload Image.</div>";
+                            header('location:'.SITEURL."admin/add-food.php");
+
                             die();
                         }
                     }
                 }
-                else
-                {
+                else {
                     $image_name = "";
                 }
 
                 $sql2 = "INSERT INTO tbl_food SET
                     title = '$title',
                     description = '$description',
-                    price = $price,
+                    price = '$price',
                     image_name = '$image_name'
                 ";
 
-                $res2 = mysqli_query($conn, $sql2);
+                $res2 = mysqli_query($conn, $sql);
 
-                if($res2 == true)
-                {
-                    $_SESSION['add'] = "<div class ='success'>Food Added Successfully.</div>";
-                    header('location:' .SITEURL. 'admin/manage-food.php');
+                if($res2 == true) {
+                    $_SESSION['add'] = "<div class='success'>Food Added Successfully.</div>";
+                    header('location:'.SITEURL.'admin/manage-food.php');
                 }
-                else
-                {
-                    $_SESSION['add'] = "<div class ='error'>Failed to add food</div>";
-                    header('location:' .SITEURL. 'admin/manage-food.php');
+                else {
+                    $_SESSION['add'] = "<div class='error'>Failed to Add Food.</div>";
+                    header('location:'.SITEURL.'admin/manage-food.php');
                 }
+            }
+        ?>
 
-            ?>
-
-        </div>
     </div>
+</div>
 
-<?php include('partials/footer.php'); ?>
+<?php include('partials/footer.php') ?>
