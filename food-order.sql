20eb76db7ce4e3e96a17e2dfe5c5100cc3c72c21
EdgarEvert <edgaroscar78@gmail.com>
2021-03-14 20:07:42 +0700

    database

--- food-order.sql
+++ food-order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2021 at 02:20 PM
+-- Generation Time: Mar 14, 2021 at 11:52 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -40,9 +40,7 @@ CREATE TABLE `tbl_admin` (
 
 INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
 (1, 'devin hidayat', 'devin', '21232f297a57a5a743894a0e4a801fc3'),
-(2, '', '', ''),
 (3, 'devin hidayat', 'devin', '202cb962ac59075b964b07152d234b70'),
-(4, '', '', ''),
 (5, 'devin hidayat', 'devin', '202cb962ac59075b964b07152d234b70'),
 (6, 'devin hidayat', 'devin', '202cb962ac59075b964b07152d234b70'),
 (16, 'devinhidayat', 'devin', 'admin'),
@@ -62,6 +60,15 @@ CREATE TABLE `tbl_category` (
   `active` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
+(58, 'Appetizers', 'Food_Category_368.jpg', '', ''),
+(59, 'Main Course', 'Food_Category_877.jpg', '', ''),
+(60, 'Dessert', 'Food_Category_284.jpg', '', '');
+
 -- --------------------------------------------------------
 
 --
@@ -74,10 +81,23 @@ CREATE TABLE `tbl_food` (
   `description` text NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `image_name` varchar(255) NOT NULL,
-  `category_id` int(10) UNSIGNED NOT NULL,
-  `featured` varchar(10) NOT NULL,
-  `active` varchar(10) NOT NULL
+  `category_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_food`
+--
+
+INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`) VALUES
+(54, 'Ice Cream', 'Ice Cream', '30000.00', 'Food-Name-9750.jpg', 60),
+(55, 'Lava Cake', 'Lava Cake', '30000.00', 'Food-Name-7376.jpg', 60),
+(56, 'Cheesy Toast', 'Cheesy Toast', '25000.00', 'Food-Name-4614.jpg', 60),
+(57, 'Pork Ribs', 'Pork Ribs', '70000.00', 'Food-Name-3780.jpg', 59),
+(58, 'Spaghetti Carbonara', 'Spaghetti Carbonara', '40000.00', 'Food-Name-3490.jpg', 59),
+(59, 'Chicken Wrap', 'Chicken Wrap', '35000.00', 'Food-Name-967.jpg', 59),
+(60, 'Salad', 'Salad', '25000.00', 'Food-Name-3110.jpg', 58),
+(61, 'French Fries', 'French Fries', '25000.00', 'Food-Name-4884.jpg', 58),
+(62, 'Garlic Bread', 'Garlic Bread', '35000.00', 'Food-Name-2237.jpg', 58);
 
 -- --------------------------------------------------------
 
@@ -98,6 +118,33 @@ CREATE TABLE `tbl_order` (
   `customer_email` varchar(100) NOT NULL,
   `customer_address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `gender` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `first_name`, `last_name`, `username`, `password`, `tgl_lahir`, `gender`) VALUES
+(4, 'Edgar', 'Oscar', 'edgaroscar', '202cb962ac59075b964b07152d234b70', '2021-03-16', 'Laki-laki'),
+(6, 'Edgar', 'Evert', 'edgarevert', '900150983cd24fb0d6963f7d28e17f72', '2021-03-17', 'Laki-laki'),
+(7, 'Edgar', 'Oscar', 'edgaroscar', '28b662d883b6d76fd96e4ddc5e9ba780', '2021-04-07', 'Laki-laki'),
+(8, 'Edgar', 'Oscar', 'edgaroscar', '28b662d883b6d76fd96e4ddc5e9ba780', '2021-03-28', 'Laki-laki'),
+(10, 'Edgar', 'Oscar', 'edgaroscar', '900150983cd24fb0d6963f7d28e17f72', '2021-03-30', 'Laki-laki');
 
 --
 -- Indexes for dumped tables
@@ -128,6 +175,12 @@ ALTER TABLE `tbl_order`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -141,19 +194,25 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
